07319416632bf0238c38bce6709eb2f88440ca1d
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-02 08:28:02 -0500

    updated setup

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 django-angular-scaffold
 =======================
 
+
 set of django commands to scaffold a django-angular project
+
+> Work in Progress
+
+###scaffold
+
+```
+./manage.py scaffold
+```
+
+Builds a assets folder structure 
+
+
+###startview
+
+```
+./manage.py startview <viewname>
+```
+
+creates new view, creates new styles and adds it to the import
--- setup.py
+++ setup.py
@@ -1,36 +1,17 @@
-import re
 from setuptools import setup
-
-# Read version from file
-VERSION_FILE = 'angular_scaffold/_version.py'
-version_text = open(VERSION_FILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, version_text, re.M)
-if mo:
-    version = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSION_FILE,))
 
 # Setup
 setup(
     name='django-angular-scaffold',
-    version=version,
+    version='0.0.2',
     url='https://github.com/mc706/django-angular-scaffold',
     author='Ryan McDevitt',
     author_email='mcdevitt.ryan@gmail.com',
-    license='Apache License 2.0',
-    packages=['angular_scaffold', 'angular_scaffold.management'],
+    license='MIT License',
+    packages=['angular_scaffold'],
     include_package_data=True,
     description='AngularJS Scaffolding for Django',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-    ],
+    download_url = 'https://github.com/mc706/django-angular-scaffold/tarball/0.1',
+    keywords = ['django', 'angular', 'scaffold'],
+    classifiers = [],
 )
