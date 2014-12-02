9ca6f180f626c2f19c37e6695f4285e3ce912bec
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-02 08:43:29 -0500

    updated to use versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+import re
+VERSIONFILE="angular_scaffold/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 # Setup
 setup(
     name='django-angular-scaffold',
-    version='0.0.2',
+    version=verstr,
     url='https://github.com/mc706/django-angular-scaffold',
     author='Ryan McDevitt',
     author_email='mcdevitt.ryan@gmail.com',
     license='MIT License',
-    packages=['angular_scaffold'],
+    packages=['angular_scaffold', 'angular_scaffold.management', 'angular_scaffold.management.commands'],
     include_package_data=True,
     description='AngularJS Scaffolding for Django',
     download_url = 'https://github.com/mc706/django-angular-scaffold/tarball/0.1',
