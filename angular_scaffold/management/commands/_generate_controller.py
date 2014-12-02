ad313801e8a27ccbb62b48bbea164ad3aae6d9e1
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-02 18:03:52 -0500

    fixed template bug in generate_controller

--- angular_scaffold/management/commands/_generate_controller.py
+++ angular_scaffold/management/commands/_generate_controller.py
@@ -1,12 +1,16 @@
 import os
 from string import Template
 
-_template = Template("""app.controller("${name}Controller", function ($scope, $location, $log) {
+_template = Template("""app.controller("${name}Controller", function ($$scope, $$location, $$log) {
     'use strict';
-    $log.debug("${name} Controller Initialized");
+    $$log.debug("${name} Controller Initialized");
 });""")
 
 def generate_controller(dir, name):
-    controller = "app" + os.sep + "controllers" + os.sep + name.lower() + "Service.js"
+    controller = "app" + os.sep + "controllers" + os.sep + name.lower() + "Controller.js"
+    title = name.title()
     with open(dir + os.sep + 'assets' + os.sep + controller, 'w') as f:
         f.write(_template.substitute(name=name.title()))
+
+if __name__ == "__main__":
+    generate_controller('../..', 'category')
