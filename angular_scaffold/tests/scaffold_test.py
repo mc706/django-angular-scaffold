c6c43019a730d0ccdd0c1be9b9cf1edbded7fd36
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-16 17:18:24 -0500

    updated scaffold test to use local dir

--- angular_scaffold/tests/scaffold_test.py
+++ angular_scaffold/tests/scaffold_test.py
@@ -8,7 +8,7 @@ from angular_scaffold.management.commands.helpers._generate_assets import genera
 class ScaffoldTest(unittest.TestCase):
 
     def setUp(self):
-        self.BASE_DIR = ''
+        self.BASE_DIR = os.path.dirname(__file__)
         if os.path.exists(os.path.join(self.BASE_DIR, 'assets')):
             shutil.rmtree(os.path.join(self.BASE_DIR, 'assets'))
 
