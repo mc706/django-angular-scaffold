26c3d88f7a6f5a6d72751e171949ec45bd931516
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-16 16:35:36 -0500

    updated build process

---
+++ angular_scaffold/tests/scaffold_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class ScaffoldTest(unittest.TestCase):
+
+    def test_scaffold_with_static_set(self):
+        pass
+
+    def test_scaffold_without_static_set(self):
+        pass
