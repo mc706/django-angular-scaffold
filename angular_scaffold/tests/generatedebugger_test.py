c191ceb3819adacb64584c79f5589c046806846c
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-16 17:48:32 -0500

    updated version to 0.9.12

--- angular_scaffold/tests/generatedebugger_test.py
+++ angular_scaffold/tests/generatedebugger_test.py
@@ -5,8 +5,8 @@ import os
 from angular_scaffold.management.commands.helpers._generate_assets import generate_assets
 from angular_scaffold.management.commands.helpers._generate_debugger import generate_debugger
 
-class GenerateDebuggerTest(unittest.TestCase):
 
+class GenerateDebuggerTest(unittest.TestCase):
     def setUp(self):
         self.BASE_DIR = os.path.dirname(__file__)
         if os.path.exists(os.path.join(self.BASE_DIR, 'assets')):
@@ -15,8 +15,8 @@ class GenerateDebuggerTest(unittest.TestCase):
 
     def test_scaffold(self):
         generate_debugger(self.BASE_DIR, 'Password1')
-        self.assertTrue(os.path.exists(os.path.join(self.BASE_DIR,'assets','app','config','logger.js')))
-        self.assertTrue(os.path.exists(os.path.join(self.BASE_DIR,'docs','logging.md')))
+        self.assertTrue(os.path.exists(os.path.join(self.BASE_DIR, 'assets', 'app', 'config', 'logger.js')))
+        self.assertTrue(os.path.exists(os.path.join(self.BASE_DIR, 'docs', 'logging.md')))
 
     def tearDown(self):
         shutil.rmtree(os.path.join(self.BASE_DIR, 'assets'))
