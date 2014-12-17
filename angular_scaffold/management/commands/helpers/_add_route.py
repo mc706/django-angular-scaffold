0a02997c2dc05206632b47d681bdfdf011dbba03
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-16 19:52:37 -0500

    added addroute tests

--- angular_scaffold/management/commands/helpers/_add_route.py
+++ angular_scaffold/management/commands/helpers/_add_route.py
@@ -18,6 +18,7 @@ _header = """app.config(function ($routeProvider) {
 _footer = """.otherwise({redirectTo: '/'});
 });"""
 
+
 def add_route(directory, when=None, controller=None, template=None, resolves=None):
     if not when:
         when = raw_input("When: ")
@@ -25,7 +26,7 @@ def add_route(directory, when=None, controller=None, template=None, resolves=Non
         controller = raw_input("Controller: ")
     if not template:
         template = raw_input("Template: ")
-    if not resolves:
+    if resolves is None:
         resolve_input = True
         resolves = []
         while resolve_input:
@@ -44,18 +45,10 @@ def add_route(directory, when=None, controller=None, template=None, resolves=Non
         template=template,
         resolutions=resolutions
     )
-    print new_route
     route_regex = '\.when\(.*?\}\)'
-    with open(os.path.join(directory, 'assets','app', 'config','routes.js'), 'r') as route_file:
+    with open(os.path.join(directory, 'assets', 'app', 'config', 'routes.js'), 'r') as route_file:
         data = route_file.read()
         routes = re.findall(route_regex, data, re.DOTALL)
-        print routes
     routes.append(new_route)
-    print routes
-    with open(os.path.join(directory, 'assets','app', 'config','routes.js'), 'w') as route_file:
+    with open(os.path.join(directory, 'assets', 'app', 'config', 'routes.js'), 'w') as route_file:
         route_file.write(_header + ''.join(routes) + _footer)
-    print "New Route Added"
-
-
-if __name__ == "__main__":
-    add_route('../..','/tickets/new/','TicketController','../app/views/tickets.html',['tickets'])
---
+++ angular_scaffold/tests/addroute_test.py
@@ -0,0 +1,29 @@
+import unittest
+import shutil
+import os
+
+from angular_scaffold.management.commands.helpers._generate_assets import generate_assets
+from angular_scaffold.management.commands.helpers._generate_routes import generate_routes
+from angular_scaffold.management.commands.helpers._add_route import add_route
+
+
+class AddRouteTest(unittest.TestCase):
+    def setUp(self):
+        self.BASE_DIR = os.path.dirname(__file__)
+        if os.path.exists(os.path.join(self.BASE_DIR, 'assets')):
+            shutil.rmtree(os.path.join(self.BASE_DIR, 'assets'))
+        generate_assets(self.BASE_DIR, 'test-app')
+        generate_routes(self.BASE_DIR)
+
+    def test_view(self):
+        add_route(self.BASE_DIR, '/', 'HomeController', 'views/home.html', [])
+        self.assertTrue(
+            os.path.exists(os.path.join(self.BASE_DIR, 'assets', 'app', 'config', 'routes.js')))
+        with open(os.path.join(self.BASE_DIR, 'assets', 'app', 'config', 'routes.js'), 'r') as js:
+            routes = js.read()
+            self.assertTrue('app.config(function ($routeProvider) {' in routes)
+            self.assertTrue("controller: 'HomeController'," in routes)
+            self.assertTrue("templateUrl: 'views/home.html'" in routes)
+
+    def tearDown(self):
+        shutil.rmtree(os.path.join(self.BASE_DIR, 'assets'))
